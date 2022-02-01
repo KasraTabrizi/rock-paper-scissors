0abda3e514ab71bb028dec3cf51c88e618e0f06b
Kasra Tabrizi <kasra@techwolf.be>
2022-02-01 16:22:07 +0100

    check function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let selectedPlayerGesture = "";
+let selectedComputerGesture = "";
+let statusGame = "";
+const gesturesArr = ["rock", "paper", "scissors"];
+
 //when start button is pressed
 document.getElementById("start_button").addEventListener("click", function () {
   //hide startscreen
@@ -8,12 +13,66 @@ document.getElementById("start_button").addEventListener("click", function () {
 });
 
 //choose rock, paper or scissors
+const gestureImage = document.querySelectorAll(".gesture_image");
 
-//put the selected gesture in the player gesture selected div
+for (let i = 0; i < gestureImage.length; i++) {
+  gestureImage[i].addEventListener("click", function () {
+    selectedGesture = gestureImage[i].getElementsByTagName("img")[0].alt;
+    console.log("player selected", selectedGesture);
 
-//create random gesture for pc and put the image under the pc gesture selected div
+    //put the selected gesture in the player gesture selected div
+    let playerSelectedDIV =
+      document.getElementsByClassName("player_selected")[0];
 
-//check who one
+    playerSelectedDIV.textContent = "";
+
+    let playerImage = document.createElement("img");
+    playerImage.src = `/assets/${selectedGesture}_player.svg`;
+
+    playerSelectedDIV.appendChild(playerImage);
+
+    //create random gesture for pc and put the image under the pc gesture selected div
+    selectedComputerGesture = gesturesArr[Math.round(Math.random() * 2)];
+    console.log("Computer selected", selectedComputerGesture);
+  });
+}
+
+//check who won and update status
+function playRound(selectedPlayerGesture, selectedComputerGesture) {
+  if (selectedPlayerGesture === selectedComputerGesture) {
+    statusGame = "It's a tie!";
+  } else if (
+    selectedPlayerGesture === "rock" &&
+    selectedComputerGesture === "scissor"
+  ) {
+    statusGame = "Player Wins!";
+  } else if (
+    selectedPlayerGesture === "rock" &&
+    selectedComputerGesture === "paper"
+  ) {
+    statusGame = "Computer Wins!";
+  } else if (
+    selectedPlayerGesture === "paper" &&
+    selectedComputerGesture === "rock"
+  ) {
+    statusGame = "Player Wins!";
+  } else if (
+    selectedPlayerGesture === "paper" &&
+    selectedComputerGesture === "scissor"
+  ) {
+    statusGame = "Computer Wins!";
+  } else if (
+    selectedPlayerGesture === "scissor" &&
+    selectedComputerGesture === "paper"
+  ) {
+    statusGame = "Player Wins!";
+  } else if (
+    selectedPlayerGesture === "scissor" &&
+    selectedComputerGesture === "rock"
+  ) {
+    statusGame = "Computer Wins!";
+  }
+}
 
 //update the correct score
 
