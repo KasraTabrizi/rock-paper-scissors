32c972967c87f5eb71c51fcaab530cb79f50d069
Kasra Tabrizi <kasra@techwolf.be>
2022-02-01 14:00:34 +0100

    play screen

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 //when start button is pressed
 document.getElementById("start_button").addEventListener("click", function () {
-  console.log("start");
+  //hide startscreen
+  document.getElementById("start_screen").style.display = "none";
+
+  //show playscreen
+  document.getElementById("play_screen").style.display = "block";
 });
+
+//choose rock, paper or scissors
+
+//put the selected gesture in the player gesture selected div
+
+//create random gesture for pc and put the image under the pc gesture selected div
+
+//check who one
+
+//update the correct score
+
+//add message of who won
